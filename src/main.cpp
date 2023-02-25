71250864b76e9ab1d3fb97c341bf763d09065a8e
blombler008 <blombler008@gmail.com>
2023-02-25 08:57:35 +0100

    Implemented ShiftRegister Behvior

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,29 @@
 WiFiUDP udp;
 WiFiServer server(8080);
  
+void shiftData(uint8_t data) {  
+    shiftOut(SR_DATA_PIN, SR_CLK_PIN, LSBFIRST, data);   
+	digitalWrite(SR_RCK_PIN, HIGH); 
+	digitalWrite(SR_RCK_PIN, LOW);
+}
+
 void setup() {
     Serial.begin(115200);
+
+	pinMode(SR_RCK_PIN, OUTPUT);
+	pinMode(SR_CLK_PIN, OUTPUT); 
+	pinMode(SR_DATA_PIN, OUTPUT);
+	digitalWrite(SR_RCK_PIN, LOW);
+	digitalWrite(SR_CLK_PIN, LOW);
+	digitalWrite(SR_DATA_PIN, LOW); 
+    
+	shiftData(0xaa);	
+    vTaskDelay(200);
+	shiftData(0x55);	
+    vTaskDelay(200);
+	shiftData(0);
+
+
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
@@ -17,10 +38,14 @@ void setup() {
 	Serial.println("Starting...");
     udp.begin(8888);
 }
- 
+ uint8_t data= 0;
 void loop() {
-    int packetSize = udp.parsePacket(); 
+    int packetSize = udp.parsePacket();  
+    
+    shiftData(1);
     if (packetSize) {
+         
+        shiftData(3);	
         byte buffer[packetSize];
         udp.read(buffer, packetSize);
         String message = String((char*)buffer);
@@ -37,7 +62,8 @@ void loop() {
     }
 	
 	if(server.hasClient()) {
-		Serial.println("Connecting TCP");
+        shiftData(7);	
+		Serial.println("[TCP] Connecting");
 		WiFiClient client = server.available(); 
 		
 		while(client.connected()) {   
@@ -45,15 +71,18 @@ void loop() {
 				String data = client.readString();
 				String newData = data.substring(0, data.indexOf("\n"));
 				Serial.println("[TCP] Received from Tablet: " + newData); 
-				client.print("Hello Tablet!\n");
-				client.flush(); 
-				Serial.println("[TCP] Send: Hello Tablet!"); 
-				Serial.println("[TCP] Closing"); 
-				 
-			} 
+
+                
+                Serial.println("[TCP] Sending: Hello Tablet!"); 
+                client.print("Hello Tablet!\n");
+                client.flush(); 
+                Serial.println("[TCP] Send: Hello Tablet!"); 
+                Serial.println("[TCP] Closing");   
+			}  
 		} 
 		server.stopAll(); 
 		Serial.println("[TCP] Connection Closed"); 
+        shiftData(0);	
 	}  
 	
 
